2df2525adb425bba903932865c89307856e12ec0
Kubas, Tomasz Jozef <63945311+TomK-127@users.noreply.github.com>
2023-04-27 23:57:23 -0400

    Created a schema and serialization script
    Very basic functionality

---
+++ serialization.py
@@ -0,0 +1,30 @@
+import flatbuffers
+from SpaceMap import SpaceObject, SurfacePoint, Space
+
+builder = flatbuffers.Builder(1024)
+
+# Create some SurfacePoints
+point_ids = [1, 2, 3]
+points = []
+for point_id in point_ids:
+    SurfacePoint.SurfacePointStart(builder)
+    SurfacePoint.SurfacePointAddLatitude(builder, point_id)
+    SurfacePoint.SurfacePointAddLongitude(builder, point_id)
+    points.append(SurfacePoint.SurfacePointEnd(builder))
+
+# Create a SpaceObject that contains SurfacePoints
+SpaceObject.SpaceObjectStartSurfaceVector(builder, len(points))
+for point in reversed(points):
+    builder.PrependUOffsetTRelative(point)
+points_vector = builder.EndVector(len(points))
+
+SpaceObject.SpaceObjectStart(builder)
+SpaceObject.SpaceObjectAddSurface(builder, points_vector)
+Space = SpaceObject.SpaceObjectEnd(builder)
+
+# Finish building the FlatBuffers object and write it to a file
+builder.Finish(Space)
+buf = builder.Output()
+with open('space.bin', 'wb') as f:
+    f.write(buf)
+
