4eea329d6dcb224b7fd3300ef363e55c033eefa2
Kubas, Tomasz Jozef <63945311+TomK-127@users.noreply.github.com>
2023-04-30 23:49:58 -0400

    Started adding classes for generating mock data

--- serialization.py
+++ serialization.py
@@ -1,5 +1,54 @@
 import flatbuffers
 from SpaceMap import SpaceObject, SurfacePoint, Space
+import numpy as np
+class Galaxy:
+    def __init__(self):
+        # Create member variables
+        self.objects = []
+        self.numOjbects = 0
+
+    def createObject(self, numPoints):
+        # x, y, z = self.split_sphere()
+        self.numOjbects += 1
+        distance = 45
+        NewSpaceObjct = SpaceObjct(self.numOjbects, distance, numPoints)
+        self.objects.append(NewSpaceObjct)
+        # import matplotlib.pyplot as plt
+        # # x, y, z = split_sphere()
+        # fig = plt.figure()
+        # ax = fig.add_subplot(projection='3d')
+        # ax.scatter(x, y, z)
+        # plt.show()
+
+
+class SpaceObjct:
+    def __init__(self, objId, distance, num_points):
+        self.objId = objId
+        self.distance = distance
+        self.x, self.y, self.z = self.split_sphere()
+        # return self.size
+
+    # Generate coordinates for a sphere
+    # https://stackoverflow.com/questions/73825731/how-to-generate-points-on-spherical-surface-making-equal-parts
+    def split_sphere(self, R = 1.0, horizontal_split = 36, vertical_split = 36, method="equal_angles"):
+        theta = np.linspace(0,360,horizontal_split+1)
+        if method == "equal_angles":
+            phi = np.linspace(0, 180, vertical_split+1)
+            c = np.cos(phi)
+            s = np.sin(phi)
+        elif method == "equal_area":
+            c = np.linspace(-1, 1, vertical_split+1)
+            s = 1 - c**2
+        else:
+            raise(ValueError('method must be "equal_angles" or "equal_area"'))
+        x = R * np.outer(s, np.cos(theta))
+        y = R * np.outer(s, np.sin(theta))
+        z = R * np.outer(c, np.ones(horizontal_split+1))
+        return x.flatten(), y.flatten(), z.flatten()
+
+Gal = Galaxy()
+Gal.createObject(1)
+Gal.createObject(2)
 
 # builder = flatbuffers.Builder(1024)
 builder = flatbuffers.Builder(0)
