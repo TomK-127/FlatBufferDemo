f734f71f658555a6d333225ca384b461dca19778
TomK-127 <63945311+TomK-127@users.noreply.github.com>
2023-08-27 21:58:52 -0600

    Added run time measurements

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 # from SpaceEncoder import *
 from GalaxyEncoder import *
 from Galaxy.galaxy import Galaxy
+import time
 
 
 def main():
     input_galaxy = Galaxy()
-    input_galaxy.generate_solar_systems(9)
+    input_galaxy.generate_solar_systems(16)
     file_name = 'galaxy.bin'
 
-    # SpaceEncoder.serialize(input_galaxy, file_name)
+    start_time = time.time()
     GalaxyEncoder.serialize(input_galaxy, file_name)
+    serialize_time = time.time() - start_time
+    print(f"serialize() runtime: {serialize_time} seconds")
 
+    start_time = time.time()
     output_galaxy = GalaxyEncoder.deserialize(file_name)
+    deserialize_time = time.time() - start_time
+    print(f"deserialize() runtime: {deserialize_time} seconds")
+
     output_galaxy.visualize_galaxy()
 
 
